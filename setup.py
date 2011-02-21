f186987854507f9cd82c91da0cdd4010e99d29d1
Sam DeLuca <sam@decarboxy.com>
2011-02-21 10:42:03 -0600

    added script installation to setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,20 @@ setup(
     name='proteinutils',
     version='1.0',
     #package_dir = {'':'lib'},
-    packages = ['rosettautil','rosettautil.graphics','rosettautil.protein','rosettautil.rosetta']
+    packages = ['rosettautil',
+                'rosettautil.graphics',
+                'rosettautil.protein',
+                'rosettautil.rosetta'
+                ],
+    scripts = [
+                'scripts/best_models.py',
+                'scripts/clustering.py',
+                'scripts/pdb_renumber.py',
+                'scripts/remove_loop_coords.py',
+                'scripts/score_scatter_plot.py',
+                'scripts/score_vs_rmsd.py',
+                'scripts/sequence_recovery.py',
+                'scripts/thread_pdb_from_alignment.py',
+                'scripts/top_n_percent.py'
+                ]
     )
