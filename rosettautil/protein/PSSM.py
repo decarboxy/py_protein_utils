2515d8e789598df7d93669f0254dcd03d285fbe2
Sam DeLuca <sam@decarboxy.com>
2011-06-15 12:18:53 -0500

    more features to PSSM.py

--- rosettautil/protein/PSSM.py
+++ rosettautil/protein/PSSM.py
@@ -1,10 +1,12 @@
 import Bio.PDB.Polypeptide
 from rosettautil.util import fileutil
 
+
 class pssm_map:
-	def __init__(self,path):
+	def __init__(self,path,mode="score"):
 		"""Parse a PSSM file from BLAST into a usable datastructure"""
 		self.pssmmap = {}
+		self.native_sequence = {}
 		pssmfile = fileutil.universal_open(path,'r')
 
 		
@@ -17,15 +19,21 @@ class pssm_map:
 		for line in pssmfile:
 			#print line
 			line = line.split()
-
+			#self.native_sequence.append(
 			if len(line) == 0:
 				break
 		
 			res_num = int(line[0])
 			res_id = line[1]
-		
+			self.native_sequence[res_num] = res_id
 			line_map = {}
-			for resname,score in zip(header,line[2:23]):
+			
+			if mode == "score":
+				data = line[2:23]
+			if mode == "percent":
+				data = line[22:42]
+				print data
+			for resname,score in zip(header,data):
 				line_map[resname] = int(score)
 			self.pssmmap[res_num] = line_map
 
@@ -43,7 +51,13 @@ class pssm_map:
 			return linemap[resname]
 		else:
 			raise LookupError("this isn't a residue")
+	def size(self):
+		return len(self.pssmmap)
 
+	def get_native_res(self,seqpos):
+		return self.native_sequence[seqpos]
+		
+	
 	def conserved(self,seqpos,resname):
 		"""return true if the score of a given sequence position and residue name is greater than 0"""
 		score = self.get_score(seqpos,resname)
